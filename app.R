42488b5a0ac4ad2de98c7104d36ec95725053830
PhuongThanhTrinh <ptri0004@student.monash.edu>
2022-08-20 14:49:12 +1000

    adding first heading

--- app.R
+++ app.R
@@ -25,3 +25,59 @@ ramen_ratings <- ramen_ratings %>%
          country = ifelse(country == "USA", "United States", country),
          country = ifelse(country == "UK", "United Kingdom", country))
 
+ramen_distribution <- ramen_ratings %>%
+  group_by(country) %>%
+  summarise(prop = n()/nrow(ramen_ratings)*100) %>%
+  left_join(world, c("country" = "name_long"))
+
+
+countries <- sort(unique(ramen_ratings$country))
+
+brands <- unique(ramen_ratings$brand)
+
+styles <- unique(ramen_ratings$style)
+
+ui <- fluidPage(
+  titlePanel("Ramen Reviews"),
+  h3("1. How are ramen manufacturer distributed across countries?"),
+  sidebarLayout(
+    sidebarPanel(
+      h1("Country"),
+      selectInput("country", "Select a country",
+                  choices = countries)),
+    mainPanel(
+      plotOutput("map"),
+      textOutput("proportion")
+    )
+  )
+)
+
+
+
+
+
+server <- function(input, output) {
+
+  output$map <- renderPlot({
+    ggplot(ramen_distribution) +
+      geom_sf(aes(geometry = geom, fill = prop),
+              color = "black") +
+      scale_fill_distiller(palette = "Accent") +
+      theme(legend.title = element_blank(), legend.position = "bottom")
+  })
+
+  output$proportion <- renderText({
+
+    names <- ramen_distribution %>%
+      filter(country==input$country)
+
+    prop <- names %>%
+      pull(prop) %>%
+      round(digits = 2)
+
+    glue("{input$country} accounts for {prop}% of ramen manufacturers internationally.")
+  })
+
+}
+
+shinyApp(ui = ui, server = server)
