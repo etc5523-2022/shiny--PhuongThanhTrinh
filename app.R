20980d0aab07cbabb889084ba2514ecd1c819321
PhuongThanhTrinh <ptri0004@student.monash.edu>
2022-08-20 14:44:58 +1000

    getting ramen data and world data

--- app.R
+++ app.R
@@ -1,117 +1,27 @@
 library(shiny)
 library(tidyverse)
+library(ggplot2)
+library(sf)
+library(spData)
+library(glue)
+library(plotly)
 
-ui <- fluidPage(
+ramen_ratings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-06-04/ramen_ratings.csv") %>%
+  drop_na(stars)
 
-    titlePanel("Old Faithful Geyser Data"),
+data(world, package = "spData")
 
-    h3("1. What number of bins do you stop seeing bimodality in the waiting time?"),
-    fluidRow(
-      sidebarLayout(
-          sidebarPanel(
-              sliderInput("bins",
-                          "Number of bins:",
-                          min = 1,
-                          max = 50,
-                          value = 30)
-          ),
+world <- world %>%
+  select(c(name_long, continent, geom))
 
-          mainPanel(
-             plotOutput("distPlot")
-          )
-      )
-    ),
+ramen_ratings <- ramen_ratings %>%
+  mutate(country = ifelse(country == "Hong Kong", "China", country),
+         country = ifelse(country == "Russia", "Russian Federation", country),
+         country = ifelse(country == "Holland", "Netherlands", country),
+         country = ifelse(country == "Dubai", "United Arab Emirates", country),
+         country = ifelse(country == "South Korea", "Republic of Korea", country),
+         country = ifelse(country == "Singapore", "Malaysia", country),
+         country = ifelse(country == "Sarawak", "Malaysia", country),
+         country = ifelse(country == "USA", "United States", country),
+         country = ifelse(country == "UK", "United Kingdom", country))
 
-    h3("2. How do the different geoms change the view of the data?"),
-    fluidRow(
-      sidebarLayout(
-        sidebarPanel(
-          radioButtons("geom",
-                      "Geom choice:",
-                      choices = c("geom_point",
-                                  "geom_density_2d",
-                                  "geom_density_2d_filled",
-                                  "geom_bin_2d",
-                                  "geom_hex"))
-        ),
-
-        mainPanel(
-          plotOutput("plot")
-        )
-      )
-    ),
-
-    h3("3. Is a mixture of two normal distribution good fit on eruption time?"),
-    fluidRow(
-      sidebarLayout(
-        sidebarPanel(
-          sliderInput("bins2",
-                      "Adjust the number of bins (if needed):",
-                      min = 1,
-                      max = 50,
-                      value = 30),
-          "Enter your guess for the:",
-          numericInput("p", "Mixing probability:",
-                       value = 0.35, min = 0, max = 1),
-          numericInput("mean1", "Mean of the first group:",
-                       value = 2.02),
-          numericInput("mean2", "Mean of the second group:",
-                       value = 4.27),
-          numericInput("sd1", "Standard deviation of the first group:",
-                       value = 0.24, min = 0),
-          numericInput("sd2", "Standard deviation of the second group:",
-                       value = 0.44, min = 0)
-        ),
-
-        mainPanel(
-          plotOutput("mixDistFit")
-        )
-      )
-    ),
-
-
-    fluidRow(
-      column(10,
-             div(class = "about",
-                 uiOutput('about'))
-      )
-    ),
-    includeCSS("styles.css")
-)
-
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        ggplot(faithful, aes(waiting)) +
-         geom_histogram(bins = input$bins, color = "white") +
-         theme_bw(base_size = 14) +
-         labs(x = "Waiting time", y = "Count")
-    })
-
-    output$plot <- renderPlot({
-      ggplot(faithful, aes(waiting, eruptions)) +
-        get(input$geom)() +
-        theme_bw(base_size = 14) +
-        labs(x = "Waiting time", y = "Eruption time")
-    })
-
-    output$mixDistFit <- renderPlot({
-      df <- data.frame(x = seq(min(faithful$eruptions), max(faithful$eruptions), length = 1000)) %>%
-        mutate(density = input$p * dnorm(x, input$mean1, input$sd1) +
-                         (1 - input$p) * dnorm(x, input$mean2, input$sd2))
-
-      ggplot(faithful, aes(eruptions)) +
-        geom_histogram(aes(y = stat(density)), bins = input$bins2, color = "white") +
-        geom_line(data = df, aes(x = x, y = density), color = "red", size = 2) +
-        theme_bw(base_size = 14) +
-        labs(x = "Eruption time", y = "Density")
-    })
-
-    output$about <- renderUI({
-      knitr::knit("about.Rmd", quiet = TRUE) %>%
-        markdown::markdownToHTML(fragment.only = TRUE) %>%
-        HTML()
-    })
-}
-
-shinyApp(ui = ui, server = server)
