d9db24b61926158179d0ac0466314db4136dcd07
PhuongThanhTrinh <ptri0004@student.monash.edu>
2022-08-22 21:48:06 +1000

    finalising all files

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
 # ETC5523 Assessment 1
 
-This is a **sample** app for the ETC5523 Assessment 1. 
+This is a **Ramen Rating** app for the ETC5523 Assessment 1. 
 
-* The creator of this app is **Emi Tanaka**.
-* The data used for this assessment is the `faithful` data that is in-built in R. 
+* The creator of this app is **Phuong Thanh Trinh**.
+* The data used for this assessment is the `ramen_rating.csv` data that is collected from the [Ramen Rater](https://www.theramenrater.com/resources-2/the-list/). 
 
-The goal of the app is in part explanatory (identify factors that affect what we see in a plot) and in part descriptive (describing the marginal or joint distribution of data).
+The aim of this app is to provide users information regarding different ramen reviews and how different factors such as styles can affect customers' ratings on number of brands.
 
-The link to the app can be found at [https://ebsmonash.shinyapps.io/etc5523-assessment1-sample/](https://ebsmonash.shinyapps.io/etc5523-assessment1-sample/)
+The link to the app can be found at [https://islzdu-phuong0thanh-trinh.shinyapps.io/shiny--PhuongThanhTrinh/](https://islzdu-phuong0thanh-trinh.shinyapps.io/shiny--PhuongThanhTrinh/).
--- app.R
+++ app.R
@@ -5,14 +5,12 @@ library(sf)
 library(spData)
 library(glue)
 library(plotly)
+library(leaflet)
 
 ramen_ratings <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-06-04/ramen_ratings.csv") %>%
   drop_na(stars)
 
 data(world, package = "spData")
-
-world <- world %>%
-  select(c(name_long, continent, geom))
 
 ramen_ratings <- ramen_ratings %>%
   mutate(country = ifelse(country == "Hong Kong", "China", country),
@@ -25,61 +23,80 @@ ramen_ratings <- ramen_ratings %>%
          country = ifelse(country == "USA", "United States", country),
          country = ifelse(country == "UK", "United Kingdom", country))
 
-ramen_distribution <- ramen_ratings %>%
-  group_by(country) %>%
-  summarise(prop = n()/nrow(ramen_ratings)*100) %>%
-  left_join(world, c("country" = "name_long"))
-
 
 countries <- sort(unique(ramen_ratings$country))
 
 brands <- unique(ramen_ratings$brand)
 
-styles <- unique(ramen_ratings$style)
 
 ui <- fluidPage(
-  titlePanel("Ramen Reviews"),
+  mainPanel(
+    htmlOutput("ramen")),
+titlePanel(title=div(img(src="ramen.jpg", height = 150, weight = 150, style="display: block; margin-left: auto; margin-right: auto;"))),
+  titlePanel(div("Ramen Rating", style="text-align: center;")),
 
-  h3("1. How are ramen manufacturer distributed across countries?"),
+  h3("1. How are ramen manufacturer distributed across selected countries?"),
   sidebarLayout(
     sidebarPanel(
-      h1("Country"),
-      selectInput("country", "Select a country",
+      selectInput("country", "Choose a country",
                   choices = countries)),
     mainPanel(
-      plotOutput("map"),
+      leafletOutput("map"),
       textOutput("proportion")
-    ),
+    )),
+  h3("2. What are the top 10 the highest average rated kinds of ramen based on countries?"),
 
-  h3("2. What are the top 10 the highest average rated kinds of ramen?"),
-
-  plotOutput("top10")
-  ),
-
-  h3("3. Is there any correlation between the style of ramen and the associated rate?"),
   sidebarLayout(
     sidebarPanel(
-      radioButtons(
-        "style", "Select a style",
-        choices = styles)),
+      helpText("Click here to explore top ramen variety across countries"),
+      selectizeInput("guess", "Country of origin",
+                     choices = countries)),
     mainPanel(
-      plotlyOutput("ramenstyle"))
-  )
+      plotOutput("top10"))),
 
+  h3("3. How do the styles of ramen affect raters' preference towards a particular brand?"),
+    sidebarLayout(
+      sidebarPanel(
+      radioButtons(
+        "style", "Choose a style",
+        choices = c("Cup", "Pack", "Tray", "Bowl", "Box", "Restaurant", "Can", "Bar"))),
+    mainPanel(plotlyOutput("ramenstyle"))
+    ),
+
+  fluidRow(
+    column(10,
+           div(class = "about",
+               uiOutput('about'))
+    )
+  ),
+  includeCSS("styles.css")
 )
-
-
-
 
 
 server <- function(input, output){
 
-  output$map <- renderPlot({
-    ggplot(ramen_distribution) +
-      geom_sf(aes(geometry = geom, fill = prop),
-              color = "black") +
-      scale_fill_distiller(palette = "Accent") +
-      theme(legend.title = element_blank(), legend.position = "bottom")
+  output$map <- renderLeaflet({
+
+    ramen_distribution <- ramen_ratings %>%
+      count(country) %>%
+      mutate(prop = n/sum(n)*100)
+
+    world <- world %>%
+      select(c(name_long, geom)) %>%
+      filter(name_long %in% countries)
+
+    cpal <- colorNumeric(palette = "Accent", domain = ramen_distribution$prop)
+
+    leaflet(world) %>%
+      addTiles() %>%
+      addPolygons(
+        color = ~cpal(ramen_distribution$prop),
+        stroke = FALSE,
+        smoothFactor = 0.3,
+        fillOpacity = 0.6) %>%
+      addLegend(pal = cpal,
+                values = ramen_distribution$prop,
+                position = "bottomright")
   })
 
   output$proportion <- renderText({
@@ -91,46 +108,55 @@ server <- function(input, output){
       pull(prop) %>%
       round(digits = 2)
 
-    glue("{input$country} accounts for {prop}% of ramen manufacturers internationally.")
+   glue("{input$country} accounts for {prop}% of ramen manufacturers internationally.")
   })
 
   output$top10 <- renderPlot({
-    ggplot(top10, aes(x = average_rate, y = brand, color = brand)) +
+
+    top10 <- ramen_ratings %>%
+      filter(country == input$guess)
+
+    top10 <- top10 %>%
+      group_by(variety) %>%
+      summarise(average_rate = mean(stars)) %>%
+      arrange(desc(average_rate)) %>%
+      head(10)
+
+     ggplot(top10, aes(x = average_rate,
+                      y = reorder(variety, average_rate))) +
       geom_bar(stat = "identity", fill = "orange") +
-      labs(x = "Average rate", y = "Brand") +
-      ggtitle("Top 10 highest rated ramen brands") +
+      labs(x = "Average rate", y = "Variety") +
+      ggtitle("Top 10 highest rated ramen varieties") +
       theme_classic()
   })
 
-  out <- reactive({
-    if (styles != "NA"){
-      renderPlotly({
-        g <- ggplot(plot, aes(x = average_rate, y = brand)) +
-          geom_col() +
-          labs(x = "Average rate", y = "Brand")
-        ggplotly(g)
-      })
-    }
-  })
 
   output$ramenstyle <- renderPlotly({
-    g <- ggplot(plot, aes(x = average_rate, y = brand)) +
-      geom_col() +
-      labs(x = "Average rate", y = "Brand")
-    ggplotly(g)
+
+      ratings <- ramen_ratings %>%
+        filter(style == input$style)
+
+    ratings <- ratings %>%
+      group_by(brand) %>%
+      summarise(mean = mean(stars)) %>%
+      arrange(desc(mean)) %>%
+      head(20)
+
+    e <- ggplot(ratings, aes(x = mean, y = reorder(brand, mean), fill = brand)) +
+        geom_bar(stat = "identity") +
+      labs(x = "Average ratings",
+           y = "Brands") +
+      ggtitle("Average ratings of top 20 brands based on selected styles") +
+      theme_classic()
+    ggplotly(e)
+})
+
+  output$about <- renderUI({
+    knitr::knit("about.Rmd", quiet = TRUE) %>%
+      markdown::markdownToHTML(fragment.only = TRUE) %>%
+      HTML()
   })
 }
 
-top10 <- ramen_ratings %>%
-  group_by(brand) %>%
-  summarise(average_rate = mean(stars)) %>%
-  arrange(-average_rate) %>%
-  head(10)
-
-plot <- ramen_ratings %>%
-  group_by(brand, style) %>%
-  summarise(average_rate = mean(stars)) %>%
-  arrange(-average_rate) %>%
-  head(10)
 
 shinyApp(ui = ui, server = server)
